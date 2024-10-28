9d7dfb9808b18424db30b4e3e1cbc9f7cbe40dfe
pluto476526 <ngugikibuka99@gmail.com>
2024-10-28 18:10:30 +0300

    Auto commit

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from bookshelf.models import Book
 
 # Register your models here.
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    display = ('title', 'author', 'publication_year')
+    items_filter = ('author', 'published_date')
+    search = ('title', 'author')
+
+    def pub_year(self, obj):
+        return obj.published_date.year
+    
+    pub_year.short_description = 'Publication Year'
+    pub_year.admin_order_field = 'published_date'
+
+admin.site.register(Book, BookAdmin)
