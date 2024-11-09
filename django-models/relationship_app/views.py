ed7c0d431465812ceeffdb2fc3fe324f5699b965
pluto476526 <ngugikibuka99@gmail.com>
2024-11-09 22:57:43 +0300

    Auto commit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from . models import Book, Library
 
 # Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, "relationship_app/list_books.html", context")
+
+
+class LibraryDetails(DetailView):
+    model = Library
+    template_name = 'library_books.html'
+    context_name = 'library'
+
+    def get_data(self):
+        context = super().books_in_lib()
+        context['books'] = Book.objects.filter(library=self.object)
+        return context
