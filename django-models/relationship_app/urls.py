756301aae66b4e28e4aed2ab50a11189c3877da1
pluto476526 <ngugikibuka99@gmail.com>
2024-11-09 23:36:57 +0300

    Auto commit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, LoginView, LogoutView
+from .views import list_books, LibraryDetailView, LoginView, LogoutView, register
 
 urlpatterns = [
     path('books_list/', list_books, name='books_list'),
     path('library/<int:pk>/', LibraryDetailView.as_view, name='library_details'),
     path('login/', LoginView.as_view, template_name='login'),
     path('logout/', LogoutView.as_view, template_name='logout'),
+    path('register/', views.register, name='register'),
 ]
