8c40b895e267a234a8ad1da2d4c7f9515bd1431e
pluto476526 <ngugikibuka99@gmail.com>
2024-11-15 23:40:00 +0300

    Auto commit

---
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
+
+# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
+
+    def publication_year(self, obj):
+        return obj.published_date.year
+    
+    publication_year.short_description = 'Publication Year'
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+    list_display = ('username', 'date_of_birth', 'profile_photo', 'is_staff')
+    list_editable = ('is_staff',)
+
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('username',)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Special Dates', {'fields': 'last_login'})
+    )
+
+
+admin.site.unregister(CustomUser)
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+admin.site.register(Book, BookAdmin)
---
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+
+        if not username:
+            raise ValueError('Username required')
+
+        user = self.model(username=username, date_of_birth=date_of_birth, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
