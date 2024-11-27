fee5824a9241e3406a92a4bac5b75e5733726bea
pluto476526 <ngugikibuka99@gmail.com>
2024-11-28 02:06:23 +0300

    Auto commit

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,7 +5,7 @@ from . views import ListView, DetailView, CreateView, UpdateView, DeleteView
 urlpatterns = [
     path('books/', ListView.as_view(), name='books_list'),
     path('books/<int:pk>/', DetailView.as_view, name='details'),
-    path('books/new/', CreateView.as_view(), name='new_book'),
-    path('books/<int:pk>/update/', UpdateView.as_view(), name='update_book')
-    path('books/<int:pk>/', DeleteView.as_view(), name='delete_book'),
+    path('books/create/', CreateView.as_view(), name='new_book'),
+    path('books/update/<int:pk>/', UpdateView.as_view(), name='update_book')
+    path('books/delete/<int:pk>/', DeleteView.as_view(), name='delete_book'),
 ]
