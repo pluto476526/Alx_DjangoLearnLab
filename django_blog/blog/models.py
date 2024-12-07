e46c335be66ad93366af3154ab4a579158faf739
pluto476526 <ngugikibuka99@gmail.com>
2024-12-08 00:33:04 +0300

    auto-commit

---
+++ django_blog/blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} by {self.author}'
