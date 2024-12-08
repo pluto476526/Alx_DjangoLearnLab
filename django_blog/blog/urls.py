51f2515acf4b7a0818393ace4187f502ca11a4ab
pluto476526 <ngugikibuka99@gmail.com>
2024-12-08 23:06:28 +0300

    auto-commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='add_comment'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name = 'edit_comment'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name= 'delete_comment'),
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='tagposts'),
 ]
